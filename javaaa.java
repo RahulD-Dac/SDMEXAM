089911cab772a6ae12d5e95b13218ad4fdd86392
RahulD-Dac <rahuldarade490@gmail.com>
2023-02-02 15:32:25 +0530

    raah

--- javaaa.java
+++ javaaa.java
@@ -1,3 +1,14 @@
-public static void name() {
-         gi
+
+class javaaa {
+
+         static int  linearSearch(int arr[],int x){
+                  int i = arr.length;
+                  for(int j=0;j<i;j++){
+                           if(arr[j]==x)
+                           return i;
+                  }
+         }
+         public static void main(String[] args) {
+                  System.out.println("Starting");
+         }
 }
